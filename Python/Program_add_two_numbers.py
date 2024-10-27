f9368a8b68f121f64be70f3701ea2c066c698513
Puni2001 <punithpunith2001@gmail.com>
2024-10-27 11:47:42 +0530

    Coding projects

---
+++ Python/Program_add_two_numbers.py
@@ -0,0 +1,18 @@
+# # Solution-1 (Pre-defined variables)
+
+number1 = 30
+number2 = 20
+sum = number1 + number2 
+
+print("The sum of given two number is", sum)
+
+# Solution-2 (User input)
+
+
+number1 = float(input("Enter a first number: "))
+number2 = float(input("Enter a second number "))
+
+sum = number1 + number2
+
+print("The sum of two numbers are: ", sum)
+
